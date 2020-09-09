10bb703d9119325a5be2d2a1a2fb3f70b48aa5ea
Pazu <libelulaby@gmail.com>
2020-09-09 11:45:47 -0500

    Creating a class to handle click on Apply Button

--- src/utils/ApplyOptions.js
+++ src/utils/ApplyOptions.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import {userData} from "../data";
 
-const ApplyOptions = (props) => {
-    const hasAppliedToCurrentEvent = userData.appliedEvents.indexOf(props.eventKey) !== -1;
+class ApplyOptions extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isActive: true};
+        this.eventKey = props.eventKey;
+        this.hasAppliedToCurrentEvent = userData.appliedEvents.indexOf(this.eventKey) !== -1;
 
-    return hasAppliedToCurrentEvent ? (
-        <button className="primary-button primary-button--checked" data-event-status="available" disabled={true}>
-            <i className="fas fa-check" aria-hidden={true}></i>
-            Applied
-        </button>
-    ) : (
-        <button className="primary-button" data-event-status="available">Apply</button>
-    );
-};
+        // Este enlace es necesario para hacer que `this` funcione en el callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        console.log("holi");
+        this.setState(state => ({
+            isActive: false
+        }));
+    }
+
+    render() {
+        return this.hasAppliedToCurrentEvent ? (
+            <button className="primary-button primary-button--checked" data-event-status="available" disabled={true}>
+                <i className="fas fa-check" aria-hidden={true}></i>
+                Applied
+            </button>
+        ) : (
+            <button className={`primary-button ${this.state.isActive ? '' : 'primary-button--checked'}`} data-event-status="available" onClick={this.handleClick}>
+                {this.state.isActive ? 'Apply' : 'Applied'}
+            </button>
+        );
+    }
+}
 
 export default ApplyOptions;
